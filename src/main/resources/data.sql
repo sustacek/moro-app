ab96edace9f521f9264b7d5a934dc0063d324b51
Josef Sustacek <sustacek@gmail.com>
2024-06-25 11:31:23 +0200

    Add hashed passwords for the users in the init script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,15 +3,23 @@
 
 -- NOTE: Make sure the manually inserted IDs in this file don't overlap with the sequence start
 -- , see UserEntity (defaults to 50)
+
+-- The password before hashing was 'b20942a6-6fd1-42dc-bce9-7697fd03fac0'
 INSERT INTO users
     (id,    name,       username,   password_hash) VALUES
-    (1,     'Petr',     'petr321',  'hash1' ) ON CONFLICT DO NOTHING;
+    (1,     'Petr',     'petr321',  '{bcrypt}$2a$10$VUPRfWvb2hHfXK6va04u9OMcKCxRJAluASNWRq.HDo800yaEXVVla' ) ON CONFLICT DO NOTHING;
+
+-- The password before hashing was '051b24f7-e053-4f64-b805-7b5734b72fc1'
 INSERT INTO users
     (id,    name,       username,   password_hash) VALUES
-    (2,     'Jana',     'janicka',  'hash2') ON CONFLICT DO NOTHING;
+    (2,     'Jana',     'janca',  '{bcrypt}$2a$10$F/lToBJJhVlG18h5CEBJGeBRTinoKqCdOUSu/.1SgyHkoXepF3h8.') ON CONFLICT DO NOTHING;
+
+-- The password before hashing was '8dd0cb3e-b59d-4917-aa26-e2fb0e05ba45'
+INSERT INTO users
+    (id,    name,       username,    password_hash) VALUES
+    (3,     'Olga',     'olinka111', '{bcrypt}$2a$10$VEnsrkEV.ax2JvFMCncSv.iDucK9QnCcLpbOV5zpvyjx8K69tgTQ6') ON CONFLICT DO NOTHING;
+
+-- The password before hashing was '23b4f81a-9f2f-4323-ac98-68fa90fd091d'
 INSERT INTO users
     (id,    name,       username,   password_hash) VALUES
-    (3,     'Olga',     'o111',     'hash3') ON CONFLICT DO NOTHING;
-INSERT INTO users
-    (id,    name,       username,   password_hash) VALUES
-    (4,     'Martin',   'martas',   'hash4') ON CONFLICT DO NOTHING;
+    (4,     'Martin',   'martas',   '{bcrypt}$2a$10$kv8odOqcpsPGKnQSdyd1F.4XbprwbXLdXp.hew6rAnwROD423XQrS') ON CONFLICT DO NOTHING;
