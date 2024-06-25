8130623dc88baf2435cd8be5091442095506cd9a
Josef Sustacek <sustacek@gmail.com>
2024-06-25 12:21:17 +0200

    Refactor - better service name, strip commented out code

--- src/main/java/cz/josefsustacek/moro/moroapp/SecurityConfig.java
+++ src/main/java/cz/josefsustacek/moro/moroapp/SecurityConfig.java
@@ -2,7 +2,6 @@ package cz.josefsustacek.moro.moroapp;
 
 import cz.josefsustacek.moro.moroapp.dao.UserRepository;
 import cz.josefsustacek.moro.moroapp.model.UserEntity;
-import cz.josefsustacek.moro.moroapp.service.exception.AuthException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,22 +9,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -54,9 +47,7 @@ public class SecurityConfig {
                          // require auth for everything else
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults())
-//                .formLogin(Customizer.withDefaults())
-                ;
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
@@ -69,9 +60,9 @@ public class SecurityConfig {
 }
 
 @Service
-class PostgresDbUserDetailsService implements UserDetailsService {
+class RepositoryBackedUserDetailsService implements UserDetailsService {
 
-    private static final Logger log = LoggerFactory.getLogger(PostgresDbUserDetailsService.class);
+    private static final Logger log = LoggerFactory.getLogger(RepositoryBackedUserDetailsService.class);
 
     @Autowired
     private UserRepository userRepository;
