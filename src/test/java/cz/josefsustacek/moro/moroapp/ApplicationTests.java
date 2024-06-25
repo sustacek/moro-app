2a260cc3bb766cbb942241eaac96cfb0ffde6c28
Josef Sustacek <sustacek@gmail.com>
2024-06-25 14:12:59 +0200

    Add ad least one test :-)

--- src/test/java/cz/josefsustacek/moro/moroapp/ApplicationTests.java
+++ src/test/java/cz/josefsustacek/moro/moroapp/ApplicationTests.java
@@ -1,6 +1,9 @@
 package cz.josefsustacek.moro.moroapp;
 
+import cz.josefsustacek.moro.moroapp.rest.UsersController;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,8 +11,12 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class ApplicationTests {
 
+    @Autowired
+    private UsersController controller;
+
     @Test
     void contextLoads() {
+        Assertions.assertThat(controller).isNotNull();
     }
 
 }
